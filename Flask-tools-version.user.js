eb0c9b50c6b6a7ca3a434226d4a23229a9aa8ce9
flasktools <57823342+flasktools@users.noreply.github.com>
2023-02-25 01:31:29 +0100

    Update Flask-tools-version.user.js

--- Flask-tools-version.user.js
+++ Flask-tools-version.user.js
@@ -1,8 +1,17 @@
 // ==UserScript==
 // @name         Flask-tools-version
-// @version      7.11
+// @version      1.0
 // @description  control version of Flask-tools
-// @author       moonlight900
+// @author       flasktools
+// @copyright    2021, flasktools
+// @license      MIT
+
+// @homepageURL  https://openuserjs.org/scripts/flasktools/Flask-tools-version
+// @supportURL   https://openuserjs.org/scripts/flasktools/Flask-tools-version/issues
+
+// @updateURL    https://openuserjs.org/meta/flasktools/Flask-tools-version.meta.js
+// @downloadURL  https://openuserjs.org/install/flasktools/Flask-tools-version.user.js
+
 // ==/UserScript==
 
 var latest_version = '7.11';
